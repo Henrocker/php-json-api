f10fd9946831dbe928c446bdce39785d2e77744b
Henrik <henrik@hnrk.io>
2019-11-06 20:19:58 +0100

    Add auth system and cleanup

--- wttr.php
+++ wttr.php
@@ -1,21 +1,19 @@
 <!DOCTYPE HTML>
 <?php
+	$authkeys = array("key1", "key2", "key3", "key4", "key5", "key6", "key7");		//Hier weitere Keys hinzufügen, falls neue Module eingesetzt werden!
 	$request_method = $_SERVER['REQUEST_METHOD'];
-
 	if ($request_method == 'POST') {
 		$POSTdata = json_decode(file_get_contents('php://input'), true);
-
-//debug		var_dump($POSTdata);
-
+		$authkey = (string) $POSTdata["authkey"];
 		$site = (string) $POSTdata["site"];
 		$loc = (string) $POSTdata["loc"];
 		$temp = (float) $POSTdata["temp"];
 		$pres = (float) $POSTdata["pres"];
 		$humi = (float) $POSTdata["humi"];
 		$io = (int) $POSTdata["io"];
-
-//debug		echo "<br>Loc: " . $loc . "<br>Temp: " . $temp . "<br>Pressure: " . $pres . "<br>Humidity: " . $humi . "<br>IO: " . $io . "<br>";
-
+		if (!in_array($authkey, $authkeys) || empty($authkey)) {
+	    	die("Invalid authentication key!");
+		}
 		if (!empty($site) && strlen($site) <= 0 || strlen($site) > 32) {
 			die("Allowed string length for <b>SITE</b>: 1-32 chars.");
 		}
@@ -30,38 +28,26 @@
 		}
 		if (empty($temp)) {
 			$temp = 0.0;
-
-			if (empty($pres)) {
-				$pres = 0000;
-
-				if (empty($humi)) {
-					$humi = 00;
-
-					if (empty($io)) {
-						$io = 0;
-						}
-					}
-				}
-			}
-//debug     echo "<br>Site: " . $site . "<br>Loc: " . $loc . "<br>Temp: " . $temp . "<br>Pressure: " . $pres . "<br>Humidity: " . $humi . "<br>IO: " . $io . "<br>";
-
-	$mysqli = new mysqli("<ENDPOINT>", "<USER>", "<PASS>", "<DB>");
-
-	if ($mysqli->connect_errno) {
-//debug		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-	}
-	else {
-//debug		echo "Database connection ESTABLISHED :-)!";
-//debug		echo $loc;
-//debug		echo $site;
-
-		$stmt = $mysqli->prepare("INSERT INTO wttr_station (site, loc, temp, pres, humi, io) VALUES (?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param('ssdddi', $site, $loc, $temp, $pres, $humi, $io);
-		$stmt->execute();
-		$stmt->close();
-//debug		echo $mysqli->info;
-//debug		echo "Fertich!";
-	}
+		}
+		if (empty($pres)) {
+			$pres = 0000;
+		}
+		if (empty($humi)) {
+			$humi = 00;
+		}
+		if (empty($io)) {
+			$io = 0;
+		}
+		$mysqli = new mysqli("<ENDPOINT>", "<USER>", "<PASSWORD>", "<DB>");
+		if ($mysqli->connect_errno) {
+			die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+		}
+		else {
+			$stmt = $mysqli->prepare("INSERT INTO wttr_station (site, loc, temp, pres, humi, io) VALUES (?, ?, ?, ?, ?, ?)");
+			$stmt->bind_param('ssdddi', $site, $loc, $temp, $pres, $humi, $io);
+			$stmt->execute();
+			$stmt->close();
+		}
 	}
 	else
 	{
@@ -69,5 +55,4 @@
         http_response_code(405);
 		echo "Nope! Try \"POST\" instead.";
 	}
-//debug     echo "<p>Ende!</p>";
 ?>
